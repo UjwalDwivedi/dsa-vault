188db56e0a801cbefaa476a5c0dbc5071b05c69e
Ujwal Dwivedi 00 <ujwaldwivedi172@gmail.com>
2026-08-18 05:49:27 +0530

    linkedlist intro added

--- Store.cpp
+++ Store.cpp
@@ -1622,3 +1622,112 @@ public:
 };
 
 //find-first-and-last-position-of-element-in-sorted-array t.c = O(logn) s.c = O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+	public:
+	int data;
+	Node* next;
+
+	public:
+
+	Node(int data1, Node* next1){
+		data = data1;
+		next = next1;
+	}
+
+	Node(int data1){
+		data = data1;
+		next = nullptr;
+	}
+};
+
+Node* convertarr2ll(vector<int> &arr){
+	Node* head = new Node(arr[0]);
+	Node* mover = head;
+
+	for(int i=1; i<arr.size(); i++){
+		Node* temp = new Node(arr[i]);
+		mover -> next = temp;
+		mover = temp;
+	}
+
+	return head;
+}
+
+//length of linkedlist
+
+int lengthofll(Node* head){
+	int cnt = 0;
+	Node* temp = head;
+	while(temp != nullptr){
+		temp = temp->next;
+		cnt++;
+	}
+	return cnt;
+}
+
+Node* deletehead(Node* head){
+	if(head == NULL) return head;
+	Node* temp = head;
+	head = head -> next;
+	free(temp);
+	return head;
+}
+
+Node* deletetail(Node* head){
+	if(head == NULL || head->next == NULL){
+		return head;
+	}
+	Node* temp = head;
+	while(temp -> next -> next != NULL){
+		temp = temp -> next;
+	}
+	free(temp->next);
+	temp->next = nullptr;
+	return head;
+}
+
+bool searchinll(Node* head, int val){
+	Node* temp = head;
+	while(temp != nullptr){
+		if(temp->data == val) return true;
+		temp = temp->next;
+	}
+	return false;
+}
+
+int main(){
+	vector<int> arr = {2,5,8,7};
+	Node* head = convertarr2ll(arr);
+	cout<<head->next<<endl;
+
+	//traversal in linkedlist
+	Node* temp = head;
+	while(temp != nullptr){
+		cout<<temp->data<<" ";
+		temp = temp->next;
+	}
+
+	cout<<endl;
+
+	cout<<lengthofll(head)<<endl;
+	cout<<searchinll(head,8)<<endl;
+
+	head = deletehead(head);
+	cout<<head->data<<endl;
+
+	head = deletetail(head);
+	Node* temp1 = head;
+	while(temp1 != nullptr){
+		cout<<temp1->data<<" ";
+		temp1 = temp1->next;
+	}
+
+	cout<<endl;
+
+}
+
+//Intro to linked list
